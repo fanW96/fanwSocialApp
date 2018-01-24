c0544122395ee913c5aaa481b034f5c5567e4784
fanw96 <fanw9611@qq.com>
2018-01-24 22:56:14 +0800

    UserController全部返回值修改为json格式

---
+++ src/main/java/com/fanw/socialapp/model/RelationJson.java
@@ -0,0 +1,44 @@
+package com.fanw.socialapp.model;
+
+import java.util.List;
+
+public class RelationJson {
+    private int code;
+    private String msg;
+    private List<Relation> relationList;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public List<Relation> getRelationList() {
+        return relationList;
+    }
+
+    public void setRelationList(List<Relation> relationList) {
+        this.relationList = relationList;
+    }
+
+    public RelationJson(int code, String msg, List<Relation> relationList) {
+        this.code = code;
+        this.msg = msg;
+        this.relationList = relationList;
+    }
+
+    public RelationJson(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+}
