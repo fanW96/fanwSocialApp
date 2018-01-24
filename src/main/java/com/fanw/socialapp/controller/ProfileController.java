04a5688aaedb48215b82da2746ca4461f7f021bb
fanw96 <fanw9611@qq.com>
2018-01-24 22:27:02 +0800

    部分返回数据尝试修改为json格式

--- src/main/java/com/fanw/socialapp/controller/ProfileController.java
+++ src/main/java/com/fanw/socialapp/controller/ProfileController.java
@@ -2,8 +2,10 @@ package com.fanw.socialapp.controller;
 
 
 import com.fanw.socialapp.model.Profile;
+import com.fanw.socialapp.model.ProfileJson;
 import com.fanw.socialapp.model.User;
 import com.fanw.socialapp.service.ProfileService;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +20,7 @@ public class ProfileController {
 
     /*
     * 注册用户的时候，创建与之对应的profile元组
+    * @param user_id
     * */
     @RequestMapping(value = "/createProfile",method = RequestMethod.POST)
     public String createProfileByUserId(User user){
@@ -32,15 +35,29 @@ public class ProfileController {
         }
         return "fail";
     }
+
     /*
     * 依靠user_id查询出相应的profile信息并且全部返回
+    * @param user_id
     * */
     @RequestMapping(value = "/showProfile",method = RequestMethod.POST)
     public String showProfileByUserId(User user){
-        return profileService.showProFileByUserId(user).toString();
+        String callback = new String();
+        Profile temp = new Profile();
+        try {
+            temp = profileService.showProFileByUserId(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(temp != null){
+            callback = new Gson().toJson(new ProfileJson(200,"success",temp));
+        }
+        return callback;
     }
+
     /*
     * 依靠profile_id查询出对应的profile元组，根据传来的profile的信息做判断来更新数据的内容
+    * @param profile_id,profile_sign,profile_sex,profile_location
     * */
     @RequestMapping(value = "/updateProfile",method = RequestMethod.POST)
     public String updateProfileByProfileId(Profile profile){
